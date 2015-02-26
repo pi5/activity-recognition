d86c16eaa6a87ca95196eb06b9b7fb3b82d30677
Piyush Waradpande <pi5@umbc.edu>
2015-02-26 15:54:00 -0500

    Visualization for collected dataset

---
+++ data_parser/parser.py
@@ -0,0 +1,54 @@
+import sys
+import json
+import math
+
+def getJSON (s):
+    j = json.loads(s)
+    return j
+    
+    
+def parsefile (s):
+    f = open(s,'r')
+    lines = f.readlines()
+    f.close()
+
+    arr = {
+        'Head':[],
+        'RightHand':[],
+        'Neck':[],
+        'RightElbow':[],
+        'RightHip':[],
+        'LeftKnee':[],
+        'LeftFoot':[],
+        'LeftHip':[],
+        'RightKnee':[],
+        'LeftElbow':[],
+        'LeftShoulder':[],
+        'RightShoulder':[],
+        'LeftHand':[],
+        'RightFoot':[],
+    }
+
+    for idx,line in enumerate(lines):
+        j = getJSON(line)
+        #print json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))
+        for joint in j['skeleton_data']['skeleton']:
+            disp = 0
+            for i in range(0,3):
+                #print "\t", j['skeleton_data']['skeleton'][joint][i], "\t", j['skeleton_data']['maxDisplacement'][joint][i] 
+                disp = disp + math.pow(j['skeleton_data']['maxDisplacement'][joint][i], 2)
+                
+            arr[joint].append([idx,disp])
+
+    print arr
+    with open('Visualization/max_displacement.json','w') as outfile:
+        json.dump(arr, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
+
+    print "File max_displacement.json written: Visualization/max_displacement.json"
+
+def main():
+    f = "../recorded_data/Feb19_2_19pm/wave_right_hand.txt"
+    parsefile(f)
+
+
+main()
